2568a9d65ca7f1b4674d3e991944c4f30357b900
gaoyi <577981827@qq.com>
2021-07-28 21:13:23 +0800

    refactor(cg): separate algo into a independent repo

--- CG/Bresenham-line-drawing-algorithm-master/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'bresenham-line-algorithm' {
-
-    export function bresenhamLinePoints(startX: number, startY: number, endX: number, endY: number): Array<object>;    
-}
--- CG/DDALineDrawing-master/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'dda-line-algorithm' {
-
-    export function DDALine(startX: number, startY: number, endX: number, endY: number): Array<object>;    
-}
